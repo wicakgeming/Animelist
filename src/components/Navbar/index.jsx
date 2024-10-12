9027c97114d6d2a538a134be3aa3ad564198dac3
wicakgeming <DimasW17@gmail.com>
2024-10-13 02:11:59 +0700

    animelist third commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <header className="bg-accent">
             <div className="flex md:flex-row sm:flex-col justify-between md:items-center p-4 gap-2">
-            <Link href="/" className="font-bold text-2xl">Animelist</Link>
+            <Link href="/" className="font-bold text-2xl">CuLAnimelist</Link>
             <InputSearch />
             <UserActionButton/>
             </div>
